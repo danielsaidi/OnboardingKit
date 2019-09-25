4d8147e95ea432ab39e5c31ac41fde46394259ee
Daniel Saidi <daniel.saidi@gmail.com>
2019-09-25 10:44:49 +0200

    Make Package use HTTPS instead of SSH

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
             targets: ["Tutti"])
     ],
     dependencies: [
-        .package(url: "git@github.com:Quick/Quick.git", from: "2.1.0"),
-        .package(url: "git@github.com:Quick/Nimble.git", .exact("8.0.2"))
+        .package(url: "https://github.com/Quick/Quick.git", from: "2.1.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", .exact("8.0.2"))
     ],
     targets: [
         .target(
