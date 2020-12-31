d5319a85ff512c6052508a6c09a11248a8bdb417
Daniel Saidi <daniel.saidi@gmail.com>
2020-12-31 12:12:54 +0100

    Replace Mockery with MockingKit

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")),
-        .package(url: "https://github.com/danielsaidi/Mockery.git", .upToNextMajor(from: "0.6.0"))
+        .package(url: "https://github.com/danielsaidi/MockingKit.git", .upToNextMajor(from: "0.8.0"))
     ],
     targets: [
         .target(
@@ -26,6 +26,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "TuttiTests",
-            dependencies: ["Tutti", "Quick", "Nimble", "Mockery"])
+            dependencies: ["Tutti", "Quick", "Nimble", "MockingKit"])
     ]
 )
