4b1ab32b732b1b8944ea7788473017e26a2f7a9c
Daniel Saidi <daniel.saidi@gmail.com>
2022-04-30 11:00:23 +0200

    Update Fastfile with new documentation scripts

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
             targets: ["Tutti"])
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.2.0")),
         .package(url: "https://github.com/danielsaidi/MockingKit.git", .upToNextMajor(from: "0.9.4"))
