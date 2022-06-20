eb8a3b70fae3864db0c0452a8bf10b9997712bc4
Daniel Saidi <daniel.saidi@gmail.com>
2022-06-20 15:27:51 +0200

    Update unit tests

--- Package.swift
+++ Package.swift
@@ -17,9 +17,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.2.0")),
-        .package(url: "https://github.com/danielsaidi/MockingKit.git", .upToNextMajor(from: "0.9.4"))
+        .package(url: "https://github.com/danielsaidi/Quick.git", branch: "main"),
+        .package(url: "https://github.com/danielsaidi/Nimble.git", branch: "main"),
+        .package(url: "https://github.com/danielsaidi/MockingKit.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import TuttiTests
-
-var tests = [XCTestCaseEntry]()
-tests += TuttiTests.allTests()
-XCTMain(tests)
