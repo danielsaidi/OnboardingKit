3cd855c54d939f426c4c978d6a793eb365074a55
Daniel Saidi <daniel.saidi@gmail.com>
2022-09-05 21:20:42 +0200

    Push minimum iOS version to 13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Tutti",
     platforms: [
-        .iOS(.v9),
+        .iOS(.v13),
         .macOS(.v10_15),
         .tvOS(.v13),
         .watchOS(.v6)
